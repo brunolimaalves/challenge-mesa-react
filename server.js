1938cb798d97b5b2b053afba969ec8cd632cf9fa
BRUNO LIMA ALVES <brunolimaalves85@gmail.com>
2021-06-09 22:54:34 -0300

    Config express

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express')
-const { resolve, path } = require('path')
+const { resolve } = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, './build')))
-// app.use('/', express.static(
-//     resolve(
-//         __dirname , 
-//         './build'
-//     )
-// ) )
+app.use('/', express.static(
+    resolve(
+        __dirname , 
+        './app'
+    )
+) )
 
 app.listen( process.env.PORT || 3000 , error => {
     error ? console.log(error) : console.log('Server Up!')
